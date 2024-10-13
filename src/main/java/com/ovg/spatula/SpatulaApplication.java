e14b6ec78b7de3a577961490498573e541170a9a
Choi Jun Ho <jjun3511@naver.com>
2024-10-13 18:00:35 +0900

    first commit.

---
+++ src/main/java/com/ovg/spatula/SpatulaApplication.java
@@ -0,0 +1,13 @@
+package com.ovg.spatula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpatulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpatulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ovg/spatula/SpatulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovg.spatula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpatulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
